7fcec4295d5deca9a3862f677a6bc2d378a14209
Deadline狂战士 <85348390+4399King@users.noreply.github.com>
2024-01-31 15:32:09 +0800

    feat: 首页样式微调
    index首页样式微调

--- src/components/custom-nav-bar/custom-nav-bar.vue
+++ src/components/custom-nav-bar/custom-nav-bar.vue
@@ -4,27 +4,42 @@ const { safeAreaInsets } = uni.getSystemInfoSync()
 </script>
 
 <template>
-  <view class="navbar" :style="{ paddingTop: safeAreaInsets!.top  + 'px' }">
+  <view class="navbar" :style="{ marginTop: safeAreaInsets!.top  + 'px' }">
     <!-- logo文字 -->
-  
+    <view class="btn">搜索</view>
     <!-- 搜索条 -->
-    <input class="search" placeholder="搜索商品"/>
+    <input class="search" placeholder="搜索影片 / 影城" placeholder-style="color:#e8e8e8;font-size:30rpx;font-weight:500" :style="{'placeholder-color':'#000'}"/>
      
  
   </view>
 </template>
 
-<style lang="less">
+<style lang="less" scoped>
 /* 自定义导航条 */
 .navbar {
   // background-image: url(@/static/images/navigator_bg.png);
+  position: relative;
   background-color: transparent;
   background-size: cover;
   position: relative;
   display: flex;
   flex-direction: column;
-  padding-top: 20px;
 
+
+  .btn {
+    position: absolute;
+    top: 16rpx;
+    left: 406rpx;
+    width: 100rpx;
+    height: 64rpx; 
+    background-color: rgba(255, 255, 255, 0.4);
+    border-radius: 0 32rpx 32rpx 0;
+    border:1px solid rgba(0, 0, 0, 0.3);
+    font-size: 32rpx;
+    line-height: 64rpx;
+    text-align: center;
+    color: #e8e8e8;
+  }
   .search {
     display: flex;
     width:60%;
@@ -33,16 +48,14 @@ const { safeAreaInsets } = uni.getSystemInfoSync()
     padding: 0 10rpx 0 26rpx;
     height: 64rpx;
     margin: 16rpx 20rpx;
-    color: #000;
+    color: #e8e8e8;
     font-size: 28rpx;
     border-radius: 32rpx;
-    background-color: rgba(255, 255, 255, 0.5);
-    border: 1px solid red;
-    
+    background-color: rgba(255, 255, 255, 0.2);
+    border: 1px solid rgba(0, 0, 0, 0.3);
   }
-  .search::placeholder {
-      color: #000;
-    }
+
+  
 
   .icon-search {
     &::before {
